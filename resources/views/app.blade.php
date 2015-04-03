eef403464ce5a5a2929da2070affe888017394b7
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2015-04-03 08:16:21 +0100

    Rename

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,12 +29,12 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Todo</a>
+				<a class="navbar-brand" href="{{ url('/') }}">TodoApp</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
+					{{-- <li><a href="{{ url('/') }}">Home</a></li> --}}
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
