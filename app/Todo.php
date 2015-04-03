70c2ab272b92c49f8797ea00e8a52e1fba79e2a8
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2015-04-03 06:44:56 +0100

    finishing creating todos

---
+++ app/Todo.php
@@ -0,0 +1,20 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model {
+
+  protected $fillable = [
+    'name',
+    'todolist_id'
+  ];
+
+	/**
+   * Todo belongs to todolist
+   */
+  public function todolist()
+    {
+      return $this->belongsTo('App\Todolist');
+    }
+
+}
